e575232c376414997a19274a2cfeb3c8b2c72bef
michalfica <michalfica125@gmail.com>
2024-04-27 13:58:18 +0200

    simplenetwork added (but not testes ? :/

--- assigmnent4/network_for_c4.py
+++ assigmnent4/network_for_c4.py
@@ -15,36 +15,47 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(2, 10, 4, padding=1)
         self.bn1 = nn.BatchNorm2d(10)
-
         self.conv2 = nn.Conv2d(10, 5, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(5)
 
         self.fc1 = nn.Linear(5*5*6, 2)
         self.bn3 = nn.BatchNorm1d(2)
-
         # self.fc2 = nn.Linear(10, 3)
-
         self.do = nn.Dropout(0.45)
 
     def forward(self, x):
-
         x = self.conv1(x)
         x = F.relu(self.bn1(x))
-
         x = self.conv2(x)
         x = F.relu(self.bn2(x))
-        
-        # print(f"shape x = {x.shape}")
 
         x = x.view(x.shape[0], -1)
 
         x = self.fc1(x)
         # x = F.relu(self.bn3(x))
-
         x = self.do(x) # dropout
-
         # x = self.fc2(x)
+        x = nn.Softmax()(x)
+        return x
 
+    def loss(self, Out, Targets):
+      return F.cross_entropy(Out, Targets)
+
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(8, 25) # zamiast 2 wstawić 25 
+        self.bn1 = nn.BatchNorm1d(25)
+        self.fc2 = nn.Linear(25,2)
+        self.do = nn.Dropout(0.35)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(self.bn1(x))
+        x = self.do(x) 
+
+        x = self.fc2(x)
+        x = self.do(x)
         x = nn.Softmax()(x)
 
         return x
@@ -53,11 +64,11 @@ class Model(nn.Module):
       return F.cross_entropy(Out, Targets)
 
 
-def load_data():
+def load_data(task=1):
     amount_of_games = 2000 
     moves_observed  = "all"
 
-    dataset = C4DataSet(amount_of_games, moves_observed).create_data_set()
+    dataset = C4DataSet(amount_of_games, moves_observed).create_data_set(task_nr=task)
 
     batch_size = 128
     train_size, val_size, test_size = int(len(dataset)/3), int(len(dataset)/3), int(len(dataset)/3)
@@ -119,35 +130,46 @@ def train(model, data_loaders, num_of_epochs, train_loader, opt, device="cpu"):
       opt.step()
 
 def run_training(model, data_loaders):
+    # -------------------------------------- task I ---------------------------------------------
     # lr = 0.00002
     # weight_decay = 0.000001
     # momentum = 0.9
     # epochs = 20
     # _device = "cpu"
-
     # # opt = torch.optim.SGD(model.parameters(), lr=lr, weight_decay = weight_decay, momentum = momentum)
     # opt = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-
-    lr = 0.0002
-    weight_decay = 0.000001
+    # lr = 0.0002
+    # weight_decay = 0.000001
+    # momentum = 0.9
+    # epochs = 12
+    # _device = "cpu"
+    # _print_every = 20
+    # opt = torch.optim.SGD(model.parameters(), lr=lr, weight_decay = weight_decay, momentum = momentum)
+    # # opt = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    
+    # -------------------------------------------------------------------------------------------
+    # -------------------------------------- task II --------------------------------------------
+    lr = 0.00001
+    weight_decay = 0.0000001
     momentum = 0.9
-    epochs = 12
+    epochs = 3
     _device = "cpu"
-    _print_every = 20
-
     opt = torch.optim.SGD(model.parameters(), lr=lr, weight_decay = weight_decay, momentum = momentum)
-    # opt = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-
     train(model=model, data_loaders=data_loaders, num_of_epochs=epochs, train_loader=data_loaders["train"], opt=opt, device=_device)
 
 
 def find_network():
     data_loaders = load_data()
-
     model = Model()
-
     initialize_weights(model)
     run_training(model, data_loaders=data_loaders)
+    return model 
 
+
+def find_simple_network():
+    data_loaders = load_data(taks=2)
+    model = SimpleModel()
+    initialize_weights(model)
+    run_training(model, data_loaders=data_loaders)
     return model 
